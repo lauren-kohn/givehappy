cc7124dfb61a7428652d74853ebd1a41e9c8c7d2
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-18 07:04:59 -0500

    stub out actions

--- app/controllers/givers_controller.rb
+++ app/controllers/givers_controller.rb
@@ -1,5 +1,24 @@
 class Givers < ApplicationController
 
+    def index
+    end
 
+    def show
+    end
+    
+    def new
+    end
+
+    def create
+    end 
+
+    def edit 
+    end 
+
+    private
+
+    def giver_params 
+        
+    end
 
 end
