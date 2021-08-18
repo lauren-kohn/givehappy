c403b5f9ed793f67860a60244b9a4806f35772c6
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-18 07:23:48 -0500

    stub out actions

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,5 +1,29 @@
 class Gifts < ApplicationController
 
+    def index
+    end 
 
+    def show
+    end
+    
+    def new
+    end
+
+    def create 
+    end 
+
+    def edit 
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private 
+
+    def gift_params
+    end
 
 end
