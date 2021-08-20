3f98b5043c93cba81f393a4c0191dde508ced561
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-19 19:34:12 -0500

    add validations to giver model

--- app/models/giver.rb
+++ app/models/giver.rb
@@ -2,6 +2,9 @@ class Giver < ActiveRecord::Base
 
     has_many :gifts
     has_many :recipients, through: :gifts
+
+    validates :name, uniqueness: true, presence: true 
+    
     has_secure_password
 
 end
