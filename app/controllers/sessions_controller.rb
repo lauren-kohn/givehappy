f39ac82520e135479b551a4cd12fb5e9c1f763db
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-19 21:02:20 -0500

    add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
     def welcome
     end 
 
+    def create 
+        Giver.find_by(email: params[:giver][:email])
+        if @giver.try(:authenticate, params[:giver][:password])
+            session[:giver_id] = @giver.id
+            redirect_to giver_path(@giver)
+        else 
+            flash[:error] = "The credentials provided were incorrect. Please try again."
+            redirect_to login_path 
+        end 
+    end
+
     def destroy
         session.delete[:user_id]
         redirect_to '/'
