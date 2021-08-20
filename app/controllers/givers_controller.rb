0a453dd2f851be6ad00cbbc70883f43af38c7072
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-19 19:12:27 -0500

    build new action

--- app/controllers/givers_controller.rb
+++ app/controllers/givers_controller.rb
@@ -1,17 +1,18 @@
 class GiversController < ApplicationController
 
+    def new
+        @giver = Giver.new
+    end
+
+    def create
+    end 
+    
     def index
     end
 
     def show
     end
     
-    def new
-    end
-
-    def create
-    end 
-
     def edit 
     end 
 
