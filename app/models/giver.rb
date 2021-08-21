bab0df2dbe6615f1501a78a2be7729c724c944cf
lauren-kohn <lauren.kohn@soft-adv.com>
2021-08-20 19:10:16 -0500

    remove name uniqueness validation

--- app/models/giver.rb
+++ app/models/giver.rb
@@ -3,7 +3,7 @@ class Giver < ActiveRecord::Base
     has_many :gifts
     has_many :recipients, through: :gifts
 
-    validates :name, uniqueness: true, presence: true 
+    validates :name, presence: true 
     validates :email, uniqueness: true, presence: true
 
     has_secure_password
